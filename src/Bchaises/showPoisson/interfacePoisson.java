568b33644385582673594dcf615b666a59c4e1e1
Bchaises <benjamin.chaises@gmail.com>
2021-05-27 10:53:36 +0200

    ajout interface + class associé

--- src/Bchaises/showPoisson/interfacePoisson.java
+++ src/Bchaises/showPoisson/interfacePoisson.java
@@ -1,8 +1,12 @@
 package Bchaises.showPoisson;
 
-import Bchaises.DBManager;
-import Bchaises.Poisson;
+import javax.swing.*;
 
-public class interfacePoisson extends javax.swing.JFrame {
+public class interfacePoisson extends JFrame {
 
+    private JPanel panel1;
+    private JButton button2;
+    private JButton button1;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
 }
